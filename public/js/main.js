20daccabbcfa475c41b001f1efe3920b0e20a249
Pavika <54035413+pavikachaudhary@users.noreply.github.com>
2020-10-11 23:20:52 +0530

    Smooth scroll over navebar

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+    // get all the links with an ID that starts with 'sectionLink'
+    const listOfLinks = document.querySelectorAll("a[href^='#sectionLink");
+    listOfLinks.forEach(function (link) {
+      link.addEventListener('click',  () => {
+
+        listOfLinks.forEach( (link) => {
+          if (link.classList.contains('highlighted')) {
+            link.classList.remove('highlighted');
+          }
+        });
+        link.classList.add('highlighted');
+        // get the element where to scroll
+        let ref = link.href.split('#sectionLink');
+        ref = "#section" + ref[1];
+    
+        if (isIE11) {
+          window.scrollTo(0, document.querySelector(ref).offsetTop);
+        } else {
+          window.scroll({
+            behavior: 'smooth',
+            left: 0,
+            // top gets the distance from the top of the page of our target element
+            top: document.querySelector(ref).offsetTop
+          });
+        }
+      })
+    })
+  })
