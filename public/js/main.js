b3944490dc3313254cb5893b7d0c8a7d889c171b
sandy9808 <sandeepkumar070200@gmail.com>
2020-10-13 17:38:10 +0530

    Added the custom scrollbar and custom cursor

--- public/js/main.js
+++ public/js/main.js
@@ -69,3 +69,10 @@ function countdown(){
 countdown();
 
 // code for countdown timer function ends
+
+//For the custom Cursor
+const root = document.querySelector(':root');
+document.addEventListener('mousemove', (e)=>{
+	root.style.setProperty('--x', e.clientX + 'px');
+	root.style.setProperty('--y', e.clientY + 'px');
+});
