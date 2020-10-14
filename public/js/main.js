dc5794fbe616b17569238d8806ffdf176c99a140
Manish Kumar <manish15052000@gmail.com>
2020-10-14 19:21:02 +0530

    added loader

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
+const loader = document.querySelector(".loader");
+const main = document.querySelector(".main-ls");
+const ctn = document.querySelector(".ctn");
+
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = "none";
+    ctn.style.display = "none";
+    main.style.display = "block";
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 4000);
+}
+
+init();
+
 document.addEventListener("DOMContentLoaded", () => {
   const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
   // get all the links with an ID that starts with 'sectionLink'
