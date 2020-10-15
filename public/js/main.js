7440a812a87668fc0001dc1023fe3e53bd8ad272
BlackSapphire <iec2019008@iiita.ac.in>
2020-10-15 18:58:52 +0530

    Added Scroll to Top button

--- public/js/main.js
+++ public/js/main.js
@@ -115,3 +115,18 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 768px)");
 myFunction(x);
 x.addListener(myFunction);
+
+// Scroll to top button 
+$(window).scroll(function(){
+  if($(this).scrollTop() > 40){
+    $("#scrollbtn").fadeIn();
+  } else {
+    $("#scrollbtn").fadeOut();
+  }
+});
+
+$(document).ready(function(){
+  $('#scrollbtn').click(function(){
+    $('html, body').animate({scrollTop : 0}, 800);
+  });
+});
