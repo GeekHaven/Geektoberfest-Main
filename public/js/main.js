6080a8f7f412f8bdb3e81d569c13f3c8ea78ceb2
Mehul Garg <iit2019041@iiita.ac.in>
2020-10-18 13:47:00 +0530

    customised the cursor

--- public/js/main.js
+++ public/js/main.js
@@ -108,26 +108,36 @@ document.addEventListener("mousemove", (e) => {
   root.style.setProperty("--y", e.clientY + "px");
 });
 
-//Disables the cursor for touch device having width less then 768 px only
-var supportsTouch = "ontouchstart" in window || navigator.msMaxTouchPoints;
-var cursor = document.getElementById("cursordiv");
-function myFunction(x) {
-  if (x.matches && supportsTouch === true) {
-    while (cursor.firstChild) {
-      cursor.removeChild(cursor.firstChild);
-    }
-  } else {
-    var node1 = document.createElement("SPAN");
-    node1.classList.add("cursor");
-    node1.classList.add("cursor--large");
-    cursor.appendChild(node1);
 
-    var node2 = document.createElement("SPAN");
-    node2.classList.add("cursor");
-    node2.classList.add("cursor--small");
-    cursor.appendChild(node2);
-  }
+let smallcursor=document.querySelector(".cursor--darks");
+let largecursor=document.querySelector(".cursor--darkl");
+
+
+document.addEventListener("mousemove",movecursor);
+
+function movecursor(e){
+  const root = document.querySelector(":root");
+document.addEventListener("mousemove", (e) => {
+  root.style.setProperty("--x", e.clientX + "px");
+  root.style.setProperty("--y", e.clientY + "px");
+});
 }
+
+let links=Array.from(document.querySelectorAll("a"));
+if(t){
+links.forEach((link)=>{
+  link.addEventListener("mouseover",()=>{
+
+    smallcursor.classList.add("grow");
+    //smallcursor.classList.remove("cursor--small");
+  });
+  link.addEventListener("mouseout",()=>{
+
+    smallcursor.classList.remove("grow");
+  });
+});
+}
+
 var x = window.matchMedia("(max-width: 768px)");
 myFunction(x);
 x.addListener(myFunction);
@@ -180,7 +190,7 @@ fetch(githubApiUrl)
                   //participant image is available as data.imageurl
                   participantsContainer.innerHTML +=
                     '<div class="image"><img class="image__img" src="'+
-                    data.imageurl +'" onerror="this.src='+'customUrl' +'"><div class="image__overlay image__overlay_blur"><div class="image__title">' +
+                    data.imageurl +'" onerror="this.src=  '+'customUrl' +'"><div class="image__overlay image__overlay_blur"><div class="image__title">' +
                     data.name +
                     '</div><div class="image__description"><p>' +
                     data.about +
